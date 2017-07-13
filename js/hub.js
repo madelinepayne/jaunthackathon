595c355c27c711c96692a6986049989ee86c880b
Peter Yang <petery@sv-mac006-petery.corp.jauntvr.com>
2017-07-12 17:01:36 -0700

    removed commented out lines

--- js/hub.js
+++ js/hub.js
@@ -1,14 +1,9 @@
 var mylat = 0;
 var mylng = 0;
 var cityName;
-// var sanMateoCoords = { lat: 37.5592181, lng: -122.2890126 };
-// var santaMonicaCoords = { lat: 34.017764, lng: -118.5175534 };
-// var newYorkCoords: { lat: 40.7454761, lng: -73.9949316 };
-// var londonCoords: { lat: 51.5205146, lng: -0.1104772 };
 
 
 function initPage() {
-    // getMyLocation();
     setWeather();
     renderMap();
 }
@@ -41,18 +36,6 @@ function hideTube()
     }
 }
 
-
-// function getMyLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(function(position) {
-//             mylat = position.coords.latitude;
-//             mylng = position.coords.longitude;
-//             renderMap();
-//         });
-//     } else {
-//         alert("Geolocation is not supported on your web browser.");
-//     }
-// }
 
 function renderMap() {
     myLocation = new google.maps.LatLng(mylat, mylng);
